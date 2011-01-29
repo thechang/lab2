4a68e9691a9b06fa6ed697ab99e8f9610e400224
gusta <gusta@gusta.(none)>
2011-01-28 22:22:36 -0600

    haciendo el primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab2::Application.config.secret_token = '1a54e92e9032b148db4188415d5052d90ab9cd4ed0f5e5756f32dedbe8b225990f027daf39bd0da3ce14935ce8b853913fbb07a8e72ab5c8fa10e62e86d2e249'
